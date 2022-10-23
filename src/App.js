24de41f7597ddb045974e06c8902113302732318
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2022-10-23 22:13:15 +0300

    add

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import "./App.css";
 import Todo from "./components/Todo";
 
 function App() {
-	return (
-		<div className="App">
-			{/* <Counter /> */}
-			<Todo />
-		</div>
-	);
+  return (
+    <div className="App">
+      {/* <Counter /> */}
+      <Todo />
+    </div>
+  );
 }
 
 export default App;
